8b97ddab410d98721c2525ff6808c4988b6e91a9
avinash-here <avinash.softer@gmail.com>
2022-10-10 21:49:41 +0530

    update

---
+++ p1_maps/src/pack3/ClassOne.java
@@ -0,0 +1,39 @@
+package pack3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ClassOne {
+    
+    public static void main(String[] args) {
+        
+        // java.util.Collections class
+        
+        List<String> colors = Arrays.asList("white", "red", "yellow", "pink", "red");
+        
+//        int res = Collections.frequency(colors, "while");
+        
+        System.out.println(Collections.frequency(colors, "while"));    // frequency method
+        System.out.println(Collections.frequency(colors, "white"));
+        System.out.println(Collections.frequency(colors, "red"));
+        
+        Collections.sort(colors);                                      // sort method
+        
+        System.out.println(colors);
+        
+        List<Student> students = new ArrayList<>();
+        
+        students.add(new Student("Aman", 10, 320));
+        students.add(new Student("Akshay", 12, 420));
+        students.add(new Student("Raghav", 23, 650));
+        students.add(new Student("Vikas", 56, 210));
+        students.add(new Student("Pradeep", 32, 560));
+        
+        Collections.sort(students);
+        
+        System.out.println(students);
+    }
+
+}
---
+++ p1_maps/src/pack3/Student.java
@@ -0,0 +1,56 @@
+package pack3;
+
+public class Student implements Comparable<Student>{
+    
+    private String name;
+    private int roll;
+    private int marks;
+    
+    public Student() {}
+
+    public Student(String name, int roll, int marks) {
+        super();
+        this.name = name;
+        this.roll = roll;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    
+    
+    
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", roll=" + roll + ", marks=" + marks + "]";
+    }
+
+    @Override
+    public int compareTo(Student st) {
+        return this.getName().compareTo(st.getName());
+    }
+    
+    
+}
