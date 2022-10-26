ea1ca7cf3c477128ec94e704b6034e92bd623b82
avinash-here <avinash.softer@gmail.com>
2022-10-26 20:16:08 +0530

    day1

---
+++ Revision/Day1/src/com/pack1/Main2.java
@@ -0,0 +1,14 @@
+package com.pack1;
+
+public class Main2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is my first Java Application");
+		System.out.println("==================================");
+		System.out.println("My Name is: Avinash Kumar");
+		System.out.println("My Address is: Purnea, Bihar");
+		
+	}
+
+}
