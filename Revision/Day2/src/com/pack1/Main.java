d049ad21328e4051a2534c418865edcdaf73209f
avinash-here <avinash.softer@gmail.com>
2022-10-27 20:18:22 +0530

    revising day2

--- Revision/Day2/src/com/pack1/Main.java
+++ Revision/Day2/src/com/pack1/Main.java
@@ -13,8 +13,33 @@ public class Main {
 		System.out.println(--a);  // 10
 		System.out.println(a);    // 10
 		
+		// Example 2
 		System.out.println(++a - ++a);  //-1
 		System.out.println(++a - a++);  // 0
+
+		// Example 3
+		System.out.println((--a + --a)*(++a - a--) + (--a + a--)*(++a + a++));  // 484
+		
+		// XOR operator: '^' is the symbol for xor operator. It returns true if both the operands are different,
+		//               otherwise it returns false.
+		
+		// AND operator
+		System.out.println(true & true);  // true
+		System.out.println(true & false); // false
+		System.out.println(false & true); // false
+		System.out.println(false & false);// false
+		
+		// OR operator
+		System.out.println(true | true);  // true
+		System.out.println(true | false); // true
+		System.out.println(false | true); // true
+		System.out.println(false | false);// false
+		
+		// AND operator
+		System.out.println(true ^ true);  // false
+		System.out.println(true ^ false); // true
+		System.out.println(false ^ true); // true
+		System.out.println(false ^ false);// false
 		
 	}
 
