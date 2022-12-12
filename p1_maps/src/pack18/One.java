8fdaaf2755476101978eadf8d76a4eaa888c9bd4
aivnash-here <avinash.softer@gmail.com>
2022-12-12 22:07:24 +0530

    fake commit

--- p1_maps/src/pack18/One.java
+++ p1_maps/src/pack18/One.java
@@ -4,7 +4,7 @@ public class One {
     
     public static void main(String[] args) {
         
-        System.out.println("This is a psuedo class!");
+        System.out.println("This is a fake commit!");
         
     }
 
