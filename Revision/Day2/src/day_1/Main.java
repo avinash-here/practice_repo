d42938ecddab93872c8d00e804b308e86679656b
aivnash-here <avinash.softer@gmail.com>
2022-12-21 23:48:07 +0530

    revising day1

---
+++ Revision/Day2/src/day_1/Main.java
@@ -0,0 +1,27 @@
+package day_1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// Introduction to Java
+		
+		// Java was developed by James Gosling in 1995 in SunMicro Systems.
+		// Later it was acquired by Oracle Corporation in 2010.
+		// At first it was named "OAK", later named "Green" and finally named 'Java', inspired 
+		// from the java coffee. That's why we see a cup of coffee in its logo.
+		
+		// Java has 3 editions:
+		// JSE : Java Standard Edition
+		// JEE : Java Enterprise Edition
+		// JME : Java Micro Edition
+		
+		// JSE is the base of other two editions.
+		
+		// To develop and run java applications, one must have JDK software in his/her PC.
+		// JDK (Java Development Kit) is also known as Java Standard Development Kit.
+		
+		System.out.println("----  day 1  ----");
+	}
+
+}
