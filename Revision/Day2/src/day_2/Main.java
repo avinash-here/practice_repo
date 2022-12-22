ffad4af2f8756a8789ec4625ea95548b7d185d03
aivnash-here <avinash.softer@gmail.com>
2022-12-22 21:18:48 +0530

    revising day2

---
+++ Revision/Day2/src/day_2/Main.java
@@ -0,0 +1,31 @@
+package day_2;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// Comment Section :
+		// There are two kinds of comment sections : 
+		// Single Line Comment  and  Multiple-Line Comment
+		
+		// This is the single line comment
+		
+		/*
+		 * 
+		 * And this is multiple-line comment
+		 * 
+		 */
+		
+		// Java Language Fundamentals : 
+		// 1. Identifiers
+		// 2. Literals
+		// 3. Keywords / Reserved words
+		// 4. Operators
+		
+		int i = 10;
+		// in line 25, int is the datatype and 'i' is the identifier
+		
+		// Literals are the values that we assign to an identifier.
+	}
+
+}
