1629da5804192eb609ab02a43b03530bb0abbc86
aivnash-here <avinash.softer@gmail.com>
2022-12-22 21:44:51 +0530

    revising day2

--- Revision/Day2/src/day_2/Main.java
+++ Revision/Day2/src/day_2/Main.java
@@ -25,7 +25,27 @@ public class Main {
 		int i = 10;
 		// in line 25, int is the datatype and 'i' is the identifier
 		
+		
 		// Literals are the values that we assign to an identifier.
+		// in line 25, 10 is the literal.
+		// We have different kinds of literals : 
+		
+		// 2a Integer/Integral Literals  ->  byte, short, int, long : 5, 39, 464, etc.  &  char : 'a', 'd', etc.
+		// 2b Floating Point Literals  ->  float : 25.26f, etc.  &  double : 696.366, etc.
+		// 2c Boolean Literals  ->  boolean : true and false
+		// 2d String Literals -> String : "Avinash", "Bihar", etc.
+		
+		
+		// Keywords : Keywords are some special words that represent a specific meaning to the compiler.
+		//			  These words can't be used as an identifier.
+		//            Examples : if, else, private, public, default, etc.
+		
+		
+		// Operators : Operators are some specific symbols that perform some specific operations on the given operands.
+		// Examples : +, -, *, /, =, %, +=, -=, etc.
+		
+		System.out.println("----  day 2  ----");
+		
 	}
 
 }
