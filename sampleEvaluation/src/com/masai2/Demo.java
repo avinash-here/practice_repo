330cd92caf3f2d8e9f0cefe9e32b1d4938444a49
aivnash-here <avinash.softer@gmail.com>
2022-12-24 23:33:08 +0530

    operators

--- sampleEvaluation/src/com/masai2/Demo.java
+++ sampleEvaluation/src/com/masai2/Demo.java
@@ -1,5 +1,49 @@
 package com.masai2;
 
 public class Demo {
+	
+	public static void main(String[] args) {
+		
+		/*
+		System.out.println(true | true);
+		System.out.println(true | false);
+		System.out.println(false | true);
+		System.out.println(false | false);
+		System.out.println();
+		System.out.println(1 | 1);
+		System.out.println(1 | 1);
+		System.out.println(0 | 1);
+		System.out.println(0 | 0);
+		System.out.println();
+		System.out.println();
+
+		System.out.println(true & true);
+		System.out.println(true & false);
+		System.out.println(false & true);
+		System.out.println(false & false);
+		System.out.println();
+		System.out.println(1 & 1);
+		System.out.println(1 & 0);
+		System.out.println(0 & 1);
+		System.out.println(0 & 0);
+		*/
+		
+		System.out.println(10 > 1);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+		
 
 }
